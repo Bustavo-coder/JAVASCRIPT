55109f36704b88f80304b24dc5f61a693f5c62be
Bustavo-coder <abdbasitegberongbe@gmail.com>
2025-08-11 19:41:25 +0100

    Nested

---
+++ CODE YOUR SUMMER/NestedLoopSquare.js
@@ -0,0 +1,9 @@
+const printSquare = (range) =>{
+for(var count = 0; count < range ; count++){
+		let sum = ""
+	for (var counter = 0;counter < range;counter++) sum+= "*"
+	console.log(sum)
+ 
+	}
+}
+printSquare(5)
