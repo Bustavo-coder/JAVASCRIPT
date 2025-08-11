89dc17df1046039bd4a5a1133813d3a8bcb90e3e
Bustavo-coder <abdbasitegberongbe@gmail.com>
2025-08-11 15:01:21 +0100

    square

---
+++ CODE YOUR SUMMER/squareNumbers.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')()
+
+const getSquare = (numb)=>{
+	for(let count = 1; count <= numb;count++){
+		console.log(`${count} squared is ${count * count}`)
+			
+		}
+	}
+
+ getSquare(5)
