e44652c9a002467ca1df251a4b4e5b82afb31646
Bustavo-coder <abdbasitegberongbe@gmail.com>
2025-08-11 18:53:16 +0100

    dbs

---
+++ CODE YOUR SUMMER/divisiblebyfiveandthree.js
@@ -0,0 +1,9 @@
+function checkDivisibleby (numb){
+	for(let count = 0; count <= numb;count++){
+		if(count % 3 == 0 &&  count % 5 == 0)console.log(count)	
+		}
+}
+
+const prompt = require('prompt-sync')()
+userNumber = prompt("Enter The Ranage To Check ")
+checkDivisibleby(userNumber)
---
+++ CODE YOUR SUMMER/triangle.js
@@ -0,0 +1,12 @@
+const printTriangle = (range)=>{
+for(var count = 0; count < range;count++){
+	let sum = ""
+	for (let counter = 0; counter < count;counter++){
+		sum+="* "
+			
+		}
+	console.log(sum)
+
+}
+}
+printTriangle(10)
